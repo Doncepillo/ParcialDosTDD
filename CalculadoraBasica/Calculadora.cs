f906ec5bde631cde55ffd346db4608bad72c1107
Doncepillo <daniel.perezz@correoaiep.cl>
2018-04-23 02:52:50 -0300

    envio

---
+++ CalculadoraBasica/Calculadora.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraBasica
+{
+    public class Calculadora
+    {
+        public int Sumar(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+
+        public int SumarCaracteres(string v1, string v2)
+        { 
+            return Convert.ToInt16(v1) + Convert.ToInt16( v2);
+        }
+
+        public int SumarCaracteresB(string v1, string v2)
+        {
+            if (v1.Equals(""))
+                {
+                v1 = "0";
+                }
+            else
+                {
+                Convert.ToInt16(v1);
+                }
+            if (v2.Equals(""))
+            {
+                v2 = "0";
+            }
+            else
+            {
+                Convert.ToInt16(v2);
+            }
+            return Convert.ToInt16(v1 + v2);
+        }
+
+        public int Restar(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+
+        public int Multiplicar(int v1, int v2)
+        {
+            return v1 * v2;
+        }
+
+        public int MultiplicarB(string v1, int v2)
+        {
+            return (v1.Equals("")) ? 0 * v2 : Convert.ToInt16(v1) * v2;
+        }
+
+        public int RaizCuadrada(int v1, int v2)
+        {
+            return  v1 ^ (v2);
+        }
+
+        public double Potencia(int v1, int v2)
+        {
+            return Math.Pow(v1, v2); 
+        }
+        
+        public int Dividir(int v1, int v2)
+        {
+            return v1 / v2;
+        }
+
+        public double DividirB(int v1, int v2)
+        {
+            double d = v1 / v2;
+            return Math.Floor(d);
+        }
+
+        public int DividirC(int v1, int v2)
+        {
+           return v1 % v2;
+        }
+    }
+}
---
+++ ParcialSimpleTest/ParcialSimpleTest.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculadoraBasica;
+
+
+namespace ParcialSimpleTest
+{
+    [TestClass]
+    public class ParcialSimpleTest
+    {
+        [TestMethod]
+        public void EjercicioUno()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.Sumar(2, 2);
+            Assert.AreEqual(4, resultado);
+        }
+
+        [TestMethod]
+        public void EjercicioDos()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.SumarCaracteres("1", "2");
+            Assert.AreEqual(3, resultado);
+        }
+
+        [TestMethod]
+        public void EjercicioTres()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.SumarCaracteresB("", "");
+            Assert.AreEqual(0, resultado);
+        }
+
+        [TestMethod]
+        public void EjercicioCuatro()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.Restar(3, 2);
+            Assert.AreEqual(1, resultado);
+        }
+
+        [TestMethod]
+        public void EjercicioCinco()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.Sumar(2, 2);
+            Assert.AreEqual(4, resultado);
+        }
+
+        [TestMethod]
+        public void EjercicioSeis()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.MultiplicarB("", 2);
+            Assert.AreEqual(0, resultado);
+        }
+
+        [TestMethod]
+        public void EjercicioSiete()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.RaizCuadrada(3, 1/2);
+            Assert.AreEqual(3, resultado);
+        }
+
+        [TestMethod]
+        public void EjercicioOcho()
+        {
+            Calculadora cal = new Calculadora();
+            double resultado = cal.Potencia(10, 2);
+            Assert.AreEqual(100, resultado);
+        }
+
+        [TestMethod]
+        public void EjercicioNueve()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.Dividir(16, 2);
+            Assert.AreEqual(8, resultado);
+        }
+
+        [TestMethod]
+        public void EjercicioDiez()
+        {
+            Calculadora cal = new Calculadora();
+            double resultado = cal.DividirB(5,2);
+            Assert.AreEqual(2, resultado);
+        }
+
+        [TestMethod]
+        public void EjercicioOnce()
+        {
+            Calculadora cal = new Calculadora();
+            int resultado = cal.DividirC(5, 2);
+            Assert.AreEqual(1, resultado);
+        }
+    }
+}
+
